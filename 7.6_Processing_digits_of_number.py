25e4469c0d50273da7c73f3f1f2319f187c0b356
Muhammad <m22850616@gmail.com>
2026-07-22 17:53:41 +0300

    feat: Task_1

--- 7.6_Processing_digits_of_number.py
+++ 7.6_Processing_digits_of_number.py
@@ -1,6 +1,23 @@
-num = 586
-while num > 0:
-    last_digit = num % 10
-    print(last_digit, sep='*', end='#')
+num = int(input())
+original_num = num
+sum_of_digits = 0
+count_of_digits = 0
+product_of_digits = 1
+
+for i in range(len(str(num))):
+    digit = num % 10
+    sum_of_digits += digit
+    count_of_digits += 1
+    product_of_digits *= digit
+    average = sum_of_digits / count_of_digits
     num //= 10
-    print()
+
+    first_digit = original_num // (10 ** (count_of_digits - 1))
+    last_digit = original_num % 10
+    sum_digits = first_digit + last_digit
+print(sum_of_digits)
+print(count_of_digits)
+print(product_of_digits)
+print(average)
+print(first_digit)
+print(sum_digits)
