877ed4799544970a4fe9f09a2b518c733a409f5a
Muhammad <m22850616@gmail.com>
2026-08-04 19:30:14 +0300

    feat: Add String_Comparison file

---
+++ 9.8_String_Comparison.py
@@ -0,0 +1,13 @@
+print('a11' > 'a10')
+print('a9' > 'a10')
+print(max('a9', 'a10', 'a11'))
+print(min('9', '10', '11') + max('9', '10', '11'))
+
+a = input()
+b = input()
+c = input()
+d = input()
+e = min(a, b, c, d)
+f = max(a, b, c, d)
+
+print(e, f)
