34a5567d6c15d05a6ad4fbe820917b50ee4a0fd9
Muhammad <m22850616@gmail.com>
2026-08-08 17:37:47 +0300

    feat: Add 11.2_Basics_of..._lists file

---
+++ 11.2_Basics_of_working_with_lists.py
@@ -0,0 +1,20 @@
+numbers = [2, 0, 2, 5]
+letters = ['a', 'b']
+
+print(len(numbers) + len(letters))
+
+browsers = ['Firefox', 'Chrome', 'Safari', 'Yandex']
+print(browsers[2:4] + browsers[3:] + browsers[:1])
+
+browsers = ['Firefox', 'Chrome', 'Safari', 'Yandex']
+print(browsers[1][-3] + browsers[2][2])
+
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+
+print(languages[-1:-10])
+
+
+
+
+
+
