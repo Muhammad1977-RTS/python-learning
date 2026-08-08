1418b5954a3ccef3735d30dbe30b1cf04425b1ee
Muhammad <m22850616@gmail.com>
2026-08-08 12:35:18 +0300

    feat: Add 11.1_Introduction_to_lists.py file

---
+++ 11.1_Introduction_to_lists.py
@@ -0,0 +1,19 @@
+
+even_numbers = list(range(0, 10, 2))
+odd_numbers = list(range(1, 10, 2))
+s = 'abcde'
+chars = list(s) 
+print(even_numbers)
+print(odd_numbers)
+print(chars)
+
+n = int(input())
+print(list(range(n + 1)), end=' ')
+
+
+
+
+
+
+
+
